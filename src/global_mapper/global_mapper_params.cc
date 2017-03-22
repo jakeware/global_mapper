4e822017e6290bef17e9b5ab99583c9829904472
jakeware <jakeware@gmail.com>
2017-03-22 02:29:45 -0400

    Add params class.

---
+++ src/global_mapper/global_mapper_params.cc
@@ -0,0 +1,20 @@
+// Copyright 2017 Massachusetts Institute of Technology
+
+#include "global_mapper/global_mapper_params.h"
+
+namespace global_mapper {
+
+GlobalMapperParams::GlobalMapperParams()
+  : voxel_xyz0_{0.0, 0.0, 0.0},
+    voxel_xyz1_{0.0, 0.0, 0.0},
+    voxel_meters_per_pixel_{0.0, 0.0, 0.0},
+    voxel_init_value_(0.0),
+    voxel_min_range_(0.0),
+    voxel_max_range_(0.0),
+    voxel_min_z_abs_(0.0),
+    voxel_max_z_abs_(0.0),
+    voxel_min_z_rel_(0.0),
+    voxel_max_z_rel_(0.0) {
+  // nothing
+}
+}  // namespace global_mapper
---
+++ src/global_mapper/global_mapper_params.h
@@ -0,0 +1,21 @@
+// Copyright 2017 Massachusetts Institute of Technology
+#pragma once
+
+namespace global_mapper {
+class GlobalMapperParams {
+ public:
+  GlobalMapperParams();
+
+  double voxel_xyz0_[3];
+  double voxel_xyz1_[3];
+  double voxel_meters_per_pixel_[3];
+  double voxel_init_value_;
+
+  double voxel_min_range_;
+  double voxel_max_range_;
+  double voxel_min_z_abs_;
+  double voxel_max_z_abs_;
+  double voxel_min_z_rel_;
+  double voxel_max_z_rel_;
+};
+}  // namespace global_mapper
