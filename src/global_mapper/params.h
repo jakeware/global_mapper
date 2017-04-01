9164a63ed6bac14b24401f235cc132d394f20fa0
jakeware <jakeware@gmail.com>
2017-04-01 12:52:26 -0400

    Remove params.cc.

--- src/global_mapper/params.h
+++ src/global_mapper/params.h
@@ -4,9 +4,34 @@
 #include <vector>
 
 namespace global_mapper {
-class Params {
- public:
-  Params();
+struct Params {
+  Params()
+  : pixel_xy_min_{0.0, 0.0},
+    pixel_xy_max_{0.0, 0.0},
+    pixel_resolution_(0.0),
+    pixel_init_value_(0.0),
+    pixel_bound_min_(0.0),
+    pixel_bound_max_(0.0),
+    pixel_min_z_abs_(0.0),
+    pixel_max_z_abs_(0.0),
+    pixel_use_rel_flatten_(false),
+    pixel_min_z_rel_(0.0),
+    pixel_max_z_rel_(0.0),
+    voxel_xyz_min_{0.0},
+    voxel_xyz_max_{0.0},
+    voxel_resolution_{0.0},
+    voxel_init_value_(0.0),
+    voxel_bound_min_(0.0),
+    voxel_bound_max_(0.0),
+    voxel_min_z_abs_(0.0),
+    voxel_max_z_abs_(0.0),
+    voxel_use_rel_cropping_(false),
+    voxel_min_z_rel_(0.0),
+    voxel_max_z_rel_(0.0),
+    voxel_hit_inc_(0.0),
+    voxel_miss_inc_(0.0) {
+      // nothing
+    }
 
   // pixel map
   double pixel_xy_min_[2];
